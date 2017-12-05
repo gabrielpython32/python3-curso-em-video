107824f4be673235a954fd97e4af432a45c1e034
Rafael F. Torres <34285280+rafaelftorres@users.noreply.github.com>
2017-12-05 20:57:52 -0300

    update

---
+++ python-3/mundo-01/exercicios/exercicio-009.py
@@ -0,0 +1,13 @@
+n = int(input('\n Digite um número para descobrir sua tabuada: '))
+n0 = n * 0
+n1 = n * 1
+n2 = n * 2
+n3 = n * 3
+n4 = n * 4
+n5 = n * 5
+n6 = n * 6
+n7 = n * 7
+n8 = n * 8
+n9 = n * 9
+n10 = n *10
+print('\n A tabuada do número {} é:\n {} X {} = {}\n {} X {} = {}\n {} X {} = {}\n {} X {} = {}\n {} X {} = {}\n {} X {} = {}\n {} X {} = {}\n {} X {} = {}\n {} X {} = {}\n {} X {} = {}\n {} X {} = {}\n'.format(n, n, 0, n0, n, 1, n1, n, 2, n2, n, 3, n3, n, 4, n4, n, 5, n5, n, 6, n6, n, 7, n7, n, 8, n8, n, 9, n9, n, 10, n10))
