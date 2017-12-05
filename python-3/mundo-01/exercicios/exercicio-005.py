080a1646e108ec500592a3c0c3e2e56c6640b3f5
Rafael F. Torres <34285280+rafaelftorres@users.noreply.github.com>
2017-12-05 20:33:30 -0300

    Atualização

--- python-3/mundo-01/exercicios/exercicio-005.py
+++ python-3/mundo-01/exercicios/exercicio-005.py
@@ -1,4 +1,7 @@
 n = int(input('Digite um número: '))
-n1 = n - 1
-n2 = n + 1
-print('Analisando o valor {}, seu antecessor é {} e o seu sucessor é {}.'.format(n, n1, n2))
+n1 = n * 2
+n2 = n * 3
+raizq = n ** (1/2)
+print('O dobro de {} vale {}.'.format(n, n1))
+print('O triplo de {} vale {}.'.format(n, n2))
+print(('A raiz quadrada de {} é igual a {}.'.format(n, raizq)))
