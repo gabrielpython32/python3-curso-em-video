34a5679759399781a5b4b04a11da8e616b25020e
Rafael F. Torres <34285280+rafaelftorres@users.noreply.github.com>
2017-12-05 20:19:23 -0300

    Atualização

---
+++ python-3/mundo-01/exercicios/exercicio-003.py
@@ -0,0 +1,4 @@
+n1 = float(input('Digite um valor: '))
+n2 = float(input('Digite outro valor: '))
+resultado = n1 + n2
+print('A soma entre {} e {} resulta em: {}.'.format(n1,n2, resultado))
---
+++ python-3/mundo-01/exercicios/exercicio-005.py
@@ -0,0 +1,4 @@
+n = int(input('Digite um número: '))
+n1 = n - 1
+n2 = n + 1
+print('Analisando o valor {}, seu antecessor é {} e o seu sucessor é {}.'.format(n, n1, n2))
