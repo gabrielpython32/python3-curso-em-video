c696783d0c995dcd9f3d4f2f782c08d170b984b0
Rafael F. Torres <34285280+rafaelftorres@users.noreply.github.com>
2017-12-06 15:21:36 -0300

    update

--- python-3/mundo-01/desafios/desafio-003.py
+++ python-3/mundo-01/desafios/desafio-003.py
@@ -1,5 +1,5 @@
 # Crie um script em Python que leia dois números
-# e tenta mostrar a soma deles.
+# e que mostre a soma deles.
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro número: '))
 res = n1 + n2
