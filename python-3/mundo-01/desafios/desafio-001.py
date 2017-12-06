51651cce149e1a0d0ff423f12d38407688931c9a
Rafael F. Torres <34285280+rafaelftorres@users.noreply.github.com>
2017-12-06 15:24:48 -0300

    update

--- python-3/mundo-01/desafios/desafio-001.py
+++
@@ -1,5 +0,0 @@
-# Crie um script em Python que leia o nome de uma pessoa
-# e mostre uma mensagem de boas-vindas de acordo com o
-# valor digitado.
-nome = input('Digite o seu nome: ')
-print('Prazer em te conhecer, {}!'.format(nome))
--- python-3/mundo-01/desafios/desafio-003.py
+++
@@ -1,6 +0,0 @@
-# Crie um script em Python que leia dois números
-# e que mostre a soma deles.
-n1 = int(input('Digite um número: '))
-n2 = int(input('Digite outro número: '))
-res = n1 + n2
-print('A soma entre {} e {} é igual a {}'.format(n1, n2, res))
