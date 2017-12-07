6882fd970a49145a0c8ba0c1633e8b5955076d0b
Rafael F. Torres <34285280+rafaelftorres@users.noreply.github.com>
2017-12-06 23:53:22 -0300

    update

--- python-3/mundo-02/exercicios/exercicio-045.py
+++ python-3/mundo-02/exercicios/exercicio-045.py
@@ -1,23 +1,26 @@
 '''Crie um programa que faça o computador jogar Jokenpô com você.'''
 from time import sleep
 from random import randint
+print('-=' * 20)
+print('Pedra, papel e tesoura.')
+print('-=' * 20)
 print('''Selecione uma opção:
 [ 0 ] PEDRA
 [ 1 ] PAPEL
 [ 2 ] TESOURA''')
 jogador = int(input('Qual é a sua jogada? '))
 print('JO')
-sleep(1)
+sleep(0.7)
 print('KEN')
-sleep(1)
+sleep(0.7)
 print('PO!!!')
-sleep(1)
+sleep(0.4)
 computador = randint(0, 2)
 opcoes = ('PEDRA', 'PAPEL', 'TESOURA')
-print('-=' * 15)
+print('-=' * 20)
 print('O computador jogou: {}'.format(opcoes[computador]))
 print('O jogador jogou: {}'.format(opcoes[jogador]))
-print('-=' * 15)
+print('-=' * 20)
 if computador == 0:  # computador jogou PEDRA
     if jogador == 0:  # jogador jogou PEDRA
         print('RESULTADO: EMPATE!')
@@ -41,4 +44,4 @@ elif computador == 2:  # computador jogou TESOURA
         print('RESULTADO: EMPATE!')
 else:
     print('JOGADA INVÁLIDA. TENTE NOVAMENTE.')
-print('-=' * 15)
+print('-=' * 20)
