b7d6c12c56ef11535889d02853f542cfffce8cc4
Rafael F. Torres <34285280+rafaelftorres@users.noreply.github.com>
2017-12-12 17:44:14 -0300

    Adição do exercício 54

---
+++ mundo-02/exercicio-054.py
@@ -0,0 +1,20 @@
+'''
+Crie um programa que leia o ano de nascimento de sete pessoas.
+No final, mostre quantas pessoas ainda não atingiram a maioridade
+e quantas já são maiores.
+'''
+from datetime import date
+menoridade = 0
+maioridade = 0
+print('''*****************************
+* Verificador de maioridade *
+*****************************''')
+qtd = int(input('\nDigite o número de pessoas para a análise: '))
+for pessoa in range(1, qtd + 1):
+    anonasc = int(input('\nInforme o ano de nascimento da {}ª pessoa: '.format(pessoa)))
+    if date.today().year - anonasc < 21:
+        menoridade += 1
+    else:
+        maioridade += 1
+print('\n{} pessoas são menores de idade.'.format(menoridade))
+print('{} pessoas são de maior idade.'.format(maioridade))
