f32b7654c66e0c24a51e91eedfe920c5b025ad24
Rafael F. Torres <34285280+rafaelftorres@users.noreply.github.com>
2017-12-18 17:45:12 -0300

    Adição do exercício 062

---
+++ mundo-02/exercicio-062.py
@@ -0,0 +1,29 @@
+'''
+Melhore o exercício 061, perguntando para o usuário se ele quer mostrar mais alguns termos.
+O programa encerra quando ele disser que quer mostrar 0 termos.
+'''
+
+print('Progressão aritimética')
+
+n1 = int(input('Informe o primeiro termo: '))
+rz = int(input('Informe a razão da PA: '))
+nn = int(input('Informe a quantidade de termos: '))
+
+cont = 0
+
+print('\nO resultado da progressão de {} com razão {} e com {} elementos é:\n'.format(n1, rz, nn))
+
+print('{} ->'.format(n1), end=' ')
+
+while nn != 0:
+    nn = nn + cont - 1
+    while cont <= nn:
+        cont += 1
+        n1 += rz
+        print('{} ->'.format(n1), end=' ')
+    print('FIM')
+    nn = int(input('\nInforme a quantidade de termos para ver a partir desses: '))
+
+print('\nNo total, foram vistos {} termos.'.format(cont))
+
+print('\nFim do cálculo da PA.')
